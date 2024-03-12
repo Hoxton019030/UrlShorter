af2f8e749161e4d16b4ba7bc366cb0f299e32f52
Hoxton <za546321@yahoo.com.tw>
2024-03-13 03:50:49 +0800

    request param

--- src/main/java/com/hoxton/urlshorter/controller/ShorterController.java
+++ src/main/java/com/hoxton/urlshorter/controller/ShorterController.java
@@ -3,7 +3,6 @@ package com.hoxton.urlshorter.controller;
 import com.hoxton.urlshorter.request.CreateShortUrlRequest;
 import com.hoxton.urlshorter.service.ShorterService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
@@ -20,8 +19,7 @@ public class ShorterController {
     private final ShorterService shorterService;
 
     @PostMapping("")
-    public ResponseEntity<String> createShortUrl(@Validated CreateShortUrlRequest createShortUrlRequest){
-        System.out.println("createShortUrlRequest.getUrl() = " + createShortUrlRequest.getUrl());
+    public ResponseEntity<String> createShortUrl(@Validated  @RequestBody  CreateShortUrlRequest createShortUrlRequest){
         return shorterService.createShortUrl(createShortUrlRequest);
     }
     @GetMapping(value = "{shorUrl}")
