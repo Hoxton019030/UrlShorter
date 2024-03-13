eddfd774a3877d2ff455c322d9496d21d52528f0
Hoxton <hoxton019030@gmail.com>
2024-03-13 19:39:46 +0800

    @GeneratedValue(strategy = GenerationType.IDENTITY)

--- src/main/java/com/hoxton/urlshorter/entity/Url.java
+++ src/main/java/com/hoxton/urlshorter/entity/Url.java
@@ -2,6 +2,7 @@ package com.hoxton.urlshorter.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Generated;
 
 import java.util.Date;
 
@@ -10,6 +11,7 @@ import java.util.Date;
 @Table(name = "url")
 public class Url {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "origin_url")
     private String originUrl;
